7211286edac78b57699eb795c3f8bbb25f802512
s33r <acow777@gmail.com>
2018-12-20 21:31:29 -0700

    more work done on menu

--- easymenu.py
+++ easymenu.py
@@ -15,10 +15,13 @@ def main_menu():
         print('=============================================')
         command = raw_input('Enter an option >>> ')
         if command == '1':
+            '''
+            Grab interface list from ifconfig or ip a
+            '''
         elif command == '2':
             attack = attack_menu()
         elif command == '3':
-            ip = raw_input('Enter victim ip')
+            ip = raw_input('Enter victim ip: ')
         elif command == '4':
             if attack == '':
                 print('No attack selected... opening attack menu')
@@ -40,22 +43,47 @@ def attack_menu():
         if command == '1':
             dos_menu()
 
-
 def dos_menu():
     while 1:
+        print('///////////////////////////////////////////')
+        print('-----------------DOS MENU------------------')
+        print('\nchoose an option...')
+        print('1: SYN FLOOD')
+        print('2: UDP FLOOD')
+        print('3: ')
+        print('4: Go Back To Main Menu')
         print('//////////////////////////////////////////')
-        print('---------------ATTACK MENU----------------')
+        command = raw_input('(ATTACK/DOS) Enter an option >>> ')
+        if command == '1':
+            dos_menu()
+
+def arp_menu():
+    while 1:
+        print('//////////////////////////////////////////')
+        print('---------------ARP MENU----------------')
+        print('\nchoose an option...')
+        print('1: ')
+        print('2: ')
+        print('3: ')
+        print('4: Menu ..')
+        print('//////////////////////////////////////////')
+        command = raw_input('(ATTACK/ARP) Enter an option >>> ')
+        if command == '1':
+            dos_menu()
+
+def dns_menu():
+    while 1:
+        print('//////////////////////////////////////////')
+        print('---------------ARP MENU----------------')
         print('\nchoose an option...')
         print('1: DOS options')
         print('2: ARP Poisoning')
         print('3: DNS Poisoning')
         print('4: Go Back To Main Menu')
         print('//////////////////////////////////////////')
-        command = raw_input('(ATTACK) Enter an option >>> ')
+        command = raw_input('(ATTACK/DNS) Enter an option >>> ')
         if command == '1':
             dos_menu()
-def arp_menu():
-def dns_menu():
 
 if __name__ == '__main__':
     main_menu():
